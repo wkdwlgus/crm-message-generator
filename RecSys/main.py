cc2643047f34a56a4f28f86a60919083f40c0b46
장지현 <jangjihyeon@jangjihyeon-ui-MacBookAir.local>
2026-01-07 14:25:53 +0900

    Fix container host to 0.0.0.0 and specify GPU resource requirements

--- RecSys/main.py
+++ RecSys/main.py
@@ -55,4 +55,4 @@ async def recommend(request: RecommendationRequest):
         raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8001, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
